bee1585ea570c2ae7d69ed7e0d62909355828653
na3r <kholghi67@gmail.com>
2011-08-16 13:09:51 +0430

    it had a bug when you want to use it more than 1 time in the same page

--- views/calendar.php
+++ views/calendar.php
@@ -1,12 +1,11 @@
 <?php
-if(empty($this->options)){
-	Yii::app()->clientScript->registerScript('jalaliDatePicker',"
-	$('#". $this->id ."').datepicker();
+if(empty($this->options)) {
+	Yii::app()->clientScript->registerScript('jalaliDatePicker'.$this->id,"
+		$('#". $this->id ."').datepicker();
 	");
 }else {
-
-	Yii::app()->clientScript->registerScript('jalaliDatePicker',"
-	$('#". $this->id ."').datepicker(". CJavaScript::encode($this->options) .");
+	Yii::app()->clientScript->registerScript('jalaliDatePicker'.$this->id,"
+		$('#". $this->id ."').datepicker(". CJavaScript::encode($this->options) .");
 	");
 } 
 
